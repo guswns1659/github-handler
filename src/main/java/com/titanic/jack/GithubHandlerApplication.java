ea0bcb84881e71398400889eb5eafbaf484d6014
guswns1659 <guswns1659@gmail.com>
2023-03-26 14:57:59 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/titanic/jack/GithubHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.titanic.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/titanic/jack/GithubHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titanic.jack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
